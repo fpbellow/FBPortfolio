a721199b004bcc99bb8166b8514a16c6d1d2e102
Fpbellow <fpbellow@gmail.com>
2025-07-16 13:56:41 -0500

    more path corrections and file management

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ import { CSS3DRenderer, CSS3DObject } from "three/addons/renderers/CSS3DRenderer
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
-
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.1 , 3200 );
 
