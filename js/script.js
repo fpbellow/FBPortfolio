6ee16200f5c229d502063b8bf5fb9b031bdc7327
Fpbellow <fpbellow@gmail.com>
2025-08-29 14:48:10 -0500

    icon fixes

--- js/script.js
+++ js/script.js
@@ -65,7 +65,7 @@ centerCard.innerHTML =  `
     </div>
     <div class="profile-card__col profile-card__col--icons">
       <a href="https://github.com/fpbellow/" target="_blank" rel="noopener noreferrer">
-        <img class="profile-card__icon" src="/assets/Images/GitHub-Mark-ea2971cee799.png"/>
+        <img class="profile-card__icon" src="https://raw.githubusercontent.com/fpbellow/FBPortfolio/main/public/Assets/Images/GitHub-Mark-ea2971cee799.png"/>
       </a>
       <a href="https://www.linkedin.com/in/faustin-bellow-048019188/" target="_blank" rel="noopener noreferrer">
         <img class="profile-card__icon" src="https://www.svgrepo.com/show/157006/linkedin.svg"/>
